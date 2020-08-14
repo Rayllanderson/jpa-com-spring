49f31d72dd71c07231a0d49a04f4778dc3c5eb08
Rayllanderson <rayllanderson@gmail.com>
2020-08-14 04:11:51 -0300

    criação da implementação PermissaoRepositoryJPA com jpa

---
+++ src/main/java/com/ray/infrastructure/repository/PermissaoRepositoryJPA.java
@@ -0,0 +1,41 @@
+package com.ray.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Component;
+
+import com.ray.domain.entities.Permissao;
+import com.ray.domain.repository.PermissaoRepository;
+
+@Component
+public class PermissaoRepositoryJPA implements PermissaoRepository{
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Override
+    public List<Permissao> todas() {
+	return em.createQuery("from Permissao", Permissao.class).getResultList();
+    }
+
+    @Override
+    public Permissao porId(Long id) {
+	return em.find(Permissao.class, id);
+    }
+
+    @Override
+    @Transactional
+    public Permissao adicionar(Permissao p) {
+	return em.merge(p);
+    }
+
+    @Override
+    @Transactional
+    public void remover(Permissao p) {
+	em.remove(this.porId(p.getId()));
+    }
+}
