45863b0a021690145719cfb96c4930d81a8b6414
rayllanderson <rayllanderson@gmail.com>
2020-08-27 19:30:54 -0300

    Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/ray/rayfood/api/controller/CozinhaController.java
+++ src/main/java/com/ray/rayfood/api/controller/CozinhaController.java
@@ -3,11 +3,15 @@ package com.ray.rayfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ray.rayfood.api.model.CozinhasXmlWrapper;
@@ -54,6 +58,12 @@ public class CozinhaController {
 	
 	//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	return ResponseEntity.notFound().build(); //atalho do retorno acima;
+    }
+    
+    @PostMapping //requisições com método POST caíram aqui 
+    @ResponseStatus(code = HttpStatus.CREATED) //status 201, created!
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) { //pode ser void, mas whatever, eu quero que tenha corpo, entao retornei algo
+	return cozinhaRepository.adicionar(cozinha);
     }
     
 
