24f90a76f2abfd428c54ef72b9e04be69a7c2eb7
rayllanderson <rayllanderson@gmail.com>
2020-09-01 19:05:35 -0300

    ops

--- src/main/java/com/ray/rayfood/domain/repository/RestaurenteRepositoryQueries.java
+++ src/main/java/com/ray/rayfood/domain/repository/RestaurenteRepositoryQueries.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import com.ray.rayfood.domain.entities.Restaurante;
-
+/*
+ * pra quÊ essa interface? só pra caso mudar o nome lá na implemetnação nao dar erro, ok?, basicamente isso kk
+ */
 public interface RestaurenteRepositoryQueries {
 
     List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
