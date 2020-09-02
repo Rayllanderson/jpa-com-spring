cf46d20159eda0c4daa3d1becc49cdd0afe85f01
rayllanderson <rayllanderson@gmail.com>
2020-09-02 02:35:12 -0300

    esqueci o mappedBy

--- src/main/java/com/ray/rayfood/domain/entities/Cozinha.java
+++ src/main/java/com/ray/rayfood/domain/entities/Cozinha.java
@@ -36,7 +36,7 @@ public class Cozinha {
     @Column(length = 30)
     private String nome;
     
-    @JsonIgnore
-    @OneToMany
+    @JsonIgnore //ignorar na representação
+    @OneToMany (mappedBy = "cozinha")//uma cozinha tem muitos restautrantes. ou seja, um tipo de cozinha, like cozinha japonesa, pode ter no restaurante x ou y
     private List<Restaurante> restaurantes = new ArrayList<>();
 }
